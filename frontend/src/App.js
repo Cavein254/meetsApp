0bcc26ac5b55ecf8481ffe714e6732292a916a45
johnmuthua <muthuahjohn@gmail.com>
2020-03-10 16:55:07 +0300

    added state

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,46 @@
 import React, {Component} from 'react';
+import { Login, Register } from './components/';
+import Styles from './components/style.css';
 
 export default class App extends Component {
   state = {
-    name:'Travis'
+    isLogginActive:true,
   }
-  constructor(){
-    super();
-    this.state = {
-      name:'Luke'
+
+  changeState() {
+    const { isLogginActive } = this.state;
+    if (isLogginActive){
+      this.PullOut.classList.remove('right');
+      this.PullOut.classList.add("left");
+    }else {
+      this.PullOut.classList.remove('left');
+      this.PullOut.classList.add("right");
     }
-    console.log('constructor');
   }
- render() {
-   let {name} = this.state;
-    return (
-      <>
- <h1>Name: {name}</h1>
-      </>
-    )
+  render() {
+    const { isLogginActive } = this.state;
+    const current = isLogginActive ? "Register" : "Login";
+    const currentActive = isLogginActive ? "login" : "register";
+      return (
+          <div className="App">
+            <div className="login">
+              <div className="container">
+                {isLogginActive && <Login containerRef={(ref)=>{this.current = ref}}/>}
+                {!isLogginActive && <Register containerRef={(ref)=>{this.current = ref}}/>}
+              </div>
+              <PullOut current={current} containerRef={(ref)=>{this.pullout = ref}} onClick={this.changeState.bind(this)}/>
+            </div>
+          </div>
+      )
   }
+}
+
+const PullOut = props => {
+  return (
+    <div className="right-side" ref={props.containerRef} onClick={props.onClick}>
+      <div className="inner-container">
+        <div className="text">{props.current}</div>
+      </div>
+    </div>
+  )
 }
---
+++ frontend/src/components/register.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+
+export class Register extends React.Component {
+    constructor(props){
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="base-container" ref={this.props.containerRef}>
+                <div className="header">Login</div>
+                <div className="content">
+                    <div className="image">
+
+                    </div>
+                    <div className="form">
+                        <div className="form-group">
+                            <label htmlFor="username">Username</label>
+                            <input type="text" name="username" placeholder="username" />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="email">Email</label>
+                            <input type="email" name="email" placeholder="email" />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="password">Password</label>
+                            <input type="password" name="password" placeholder="password" />
+                        </div>
+                    </div>
+                    <div className="footer">
+                        <button className="btn" type="button">Register</button>
+                    </div>
+                </div>
+
+            </div>
+        )
+    }
+}
